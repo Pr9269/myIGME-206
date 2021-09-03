d2ea94aaf3862939ce9c654551780588ed25328e
Pr9269 <pr9269@g.rit.edu>
2021-09-02 20:37:33 -0400

    Q5 - PE3

---
+++ Q5 - PE3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q5___PE3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {         
+            Console.WriteLine("Enter the first number : ");
+            float a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number : ");
+            float b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the third number : ");
+            float c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the forth number : ");
+            float d = Convert.ToInt32(Console.ReadLine());
+
+            float e = a * b * c * d;
+            Console.WriteLine("The product of all 4 numbers are : " + e);
+        }
+    }
+}
