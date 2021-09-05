41afd67e5fad5cb715534bddcc92fb22c7baceab
Pr9269 <pr9269@g.rit.edu>
2021-09-05 13:32:09 -0400

    PE4-Q2

--- Q5 - PE3/Program.cs
+++ Q5 - PE3/Program.cs
@@ -9,8 +9,8 @@ namespace Q5___PE3
     class Program
     {
         static void Main(string[] args)
-        {         
-            Console.WriteLine("Enter the first number : ");
+        {
+            /*Console.WriteLine("Enter the first number : ");
             float a = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter the second number : ");
             float b = Convert.ToInt32(Console.ReadLine());
@@ -20,7 +20,14 @@ namespace Q5___PE3
             float d = Convert.ToInt32(Console.ReadLine());
 
             float e = a * b * c * d;
-            Console.WriteLine("The product of all 4 numbers are : " + e);
+            Console.WriteLine("The product of all 4 numbers are : " + e);*/
+
+            Console.WriteLine("Please type your name below.");
+            string myName = "Parth";
+            string lastName = "Rustagi";
+            string fullName = myName + lastName;
+            Console.WriteLine("Your name is " + fullName + "!");
+
         }
     }
 }
