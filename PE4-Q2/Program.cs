b873554dd46b12060551bac25531d1ab8416fc08
Pr9269 <pr9269@g.rit.edu>
2021-09-06 16:00:21 -0400

    PE4-Q2

--- PE4-Q2/Program.cs
+++ PE4-Q2/Program.cs
@@ -6,30 +6,35 @@ using System.Threading.Tasks;
 
 namespace PE4_FLOWCONTROL
 {
+    //Problem Statement - Write a console application that includes the logic from Exercise 1, that obtains two numbers from the user
+    //                    and displays them, but rejects any input where both numbers are greater than 10 and asks for two new numbers.
     class Program
     {
+        //Author - Parth Rustagi
+        //Purpose - To write the code for the console application as the solution to the listed problem
         static void Main(string[] args)
         {
-            bool xNumber = false;
-            double var1 = 0;
-            double var2 = 0;
+            double var1 = 0; //To store the value of the first number
+            double var2 = 0; //To store the value of the second number
+            bool xNumber = false; //To keep the loop running until desired input is not given
 
+            //I used while loop. This loop will end when the user enters the correct values i.e., when both the numbers are not more than 10.
             while (!xNumber)
             {
-                Console.Write("Enter the first number : ");
-                var1 = Convert.ToDouble(Console.ReadLine());
-                Console.Write("Enter the second number : ");
-                var2 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Enter the first number : "); //Displays the message to user to enter first number
+                var1 = Convert.ToDouble(Console.ReadLine()); //Asks the user for the input and then converts it into the data-type double
+                Console.Write("Enter the second number : "); //Displays the message to user to enter second number
+                var2 = Convert.ToDouble(Console.ReadLine()); //Asks the user for the input and then converts it into the data-type double
 
-                if (var1 <= 10 || var2 <= 10)
+                if (var1 <= 10 || var2 <= 10) //Checks for the condition that either both the numbers are less than 10 or any one of them is greater than 10.
                 {
                     Console.WriteLine("Numbers are accepted");
-                    xNumber = true;
+                    xNumber = true; //Will stop the while loop and terminate the program
                 }
-                else
+                else //If both the numbers entered by the users are more than 10 then this condition will be executed
                 {
                     Console.WriteLine("Both the numbers are greater than 10");
-                    Console.WriteLine("Please input the numbers again");
+                    Console.WriteLine("Please input the numbers again");//Asks the users for inputs again and will run the while loop again.
                 }
             }
         }
