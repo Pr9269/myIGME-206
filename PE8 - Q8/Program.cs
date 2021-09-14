0f1eb307940003b6840c2e223208917e622aec8c
Pr9269 <pr9269@g.rit.edu>
2021-09-13 21:03:53 -0400

    PE8 - Q9

---
+++ PE8 - Q8/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8___Q8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = null;
+
+            Console.Write("Enter a string: ");
+            str = Console.ReadLine();
+            str = str.ToLower();
+            String[] s = str.Split(' ');
+
+            string resultString = null;
+
+            foreach(string word in s)
+            {
+                if(word.CompareTo("yes") == 0)
+                {
+                    string word1;
+                    word1 = word.Replace("yes", "no");
+                    resultString += " " + word1;
+                }
+
+                else if(word.CompareTo("no") == 0)
+                {
+                    string word1;
+                    word1 = word.Replace("no", "yes");
+                    resultString += " " + word1;
+                }
+
+                else
+                {
+                    resultString += " " + word;
+                }
+
+                Console.WriteLine("String is: " + resultString);
+            }
+        }
+    }
+}
---
+++ PE8 -Q9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE8__Q9
+{
+    //Author: Parth Rustagi
+    //Purpose: To add double quotes before and after each word
+    class Program
+    {
+        //Main Method
+        //Purpose: Splitting the user input string and then print each word by using foreach function
+        static void Main(string[] args)
+        {
+            string str;
+            Console.Write("Enter a string: ");
+            str = Console.ReadLine();
+
+            String[] s = str.Split(' ');
+
+            foreach (string word in s)
+            {
+                Console.Write("\"" + word + "\"");
+            }
+        }
+    }
+}
