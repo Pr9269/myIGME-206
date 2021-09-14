3b10c6c0a5fd6577aeadf6debeaee00f0b0ef37a
Pr9269 <pr9269@g.rit.edu>
2021-09-13 21:42:02 -0400

    PE8 - Q8

--- PE8 - Q8/Program.cs
+++ PE8 - Q8/Program.cs
@@ -15,33 +15,8 @@ namespace PE8___Q8
             Console.Write("Enter a string: ");
             str = Console.ReadLine();
             str = str.ToLower();
-            String[] s = str.Split(' ');
 
-            string resultString = null;
-
-            foreach(string word in s)
-            {
-                if(word.CompareTo("yes") == 0)
-                {
-                    string word1;
-                    word1 = word.Replace("yes", "no");
-                    resultString += " " + word1;
-                }
-
-                else if(word.CompareTo("no") == 0)
-                {
-                    string word1;
-                    word1 = word.Replace("no", "yes");
-                    resultString += " " + word1;
-                }
-
-                else
-                {
-                    resultString += " " + word;
-                }
-
-                Console.WriteLine("String is: " + resultString);
-            }
+            Console.WriteLine("The resulting string is :\n " + str.Replace("no", "yes"));
         }
     }
 }
