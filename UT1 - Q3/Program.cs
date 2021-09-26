27253409ea325a8809e5e4703d04e5433d021476
Pr9269 <pr9269@g.rit.edu>
2021-09-26 18:18:11 -0400

    UT1 - Q3

---
+++ UT1 - Q3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UT1___Q3
+{
+    //Author: Parth Rustagi
+    //Purpose: To declare a delegate function to impersonate Read function
+    class Program
+    {
+        delegate string readlinefunctionDelegate();
+
+        //Main Function
+        //Purpose: Using the declared delegate taking an input from user and then displaying it. 
+        static void Main(string[] args)
+        {
+            readlinefunctionDelegate Rline = new readlinefunctionDelegate(Console.ReadLine);
+            Console.Write("Enter a string: ");
+            string newString = Rline();
+            Console.WriteLine("String entered by you was: " + newString);
+        }
+    }
+}
