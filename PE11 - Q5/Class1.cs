7f79da0c477c17364c67cbcac7a831904be8397f
Pr9269 <pr9269@g.rit.edu>
2021-09-27 15:55:01 -0400

    PE11 - Q5

---
+++ PE11 - Q5/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE11___Q5
+{
+    public class Class1
+    {
+        public virtual void LoadPassenger() { }
+
+        [+A:Vehicle|+LoadPassenger():v]
+        [+A:Car| | ]
+        [+I:IPassengerCarrier| |LoadPassenger()]
+        [+I:IHeavyLoaderCarrier| | ]
+        [+A:Train| | ]
+
+        [+Compact| | ]
+        [+SUV| | ]
+        [+Pickup| | ]
+        [+PassengerTrain| | ]
+        [+FreightTrain| | ]
+        [+424DoubleBogey| | ]
+
+        [+A:Vehicle] <- [+A:Car]
+        [+A:Vehicle] <- [+A:Train]
+
+        [+A:Car] <- [+Compact]
+        [+A:Car] <- [+SUV]
+        [+A:Car] <- [+Pickup]
+
+        [+A:Train] <- [+FreightTrain]
+        [+A:Train] <- [+424DoubleBogey]
+        [+A:Train] <- [+PassengerTrain]
+
+        [+I:IPassengerCarrier] ^ [+Compact]
+        [+I:IPassengerCarrier] ^ [+SUV]
+        [+I:IPassengerCarrier] ^ [+Pickup]
+        [+I:IPassengerCarrier] ^ [+PassengerTrain]
+
+        [+I:IHeavyLoaderCarrier] ^ [+Pickup]
+        [+I:IHeavyLoaderCarrier] ^ [+FreightTrain]
+        [+I:IHeavyLoaderCarrier] ^ [+424DoubleBogey]
+    }
+}
