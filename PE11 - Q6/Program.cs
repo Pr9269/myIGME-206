3e034f644a06315c584ceeb7c2ea21d1d4cfa7ba
Pr9269 <pr9269@g.rit.edu>
2021-09-27 23:37:53 -0400

    PE11 - Q6

--- PE11 - Q6/Program.cs
+++ PE11 - Q6/Program.cs
@@ -7,13 +7,17 @@ using PE11___Q5;
 
 namespace PE11___Q6
 {
+    //Author: Parth Rustagi
     class Program
     {
+        //Main Program
         static void Main(string[] args)
         {
             AddPassenger(new Class1.Compact());
             AddPassenger(new Class1.SUV());
         }
+
+        //AddPassenger secondary program
         static void AddPassenger(Class1.IPassengerCarrier Vehicle)
         {
             Console.WriteLine(Vehicle.ToString());
