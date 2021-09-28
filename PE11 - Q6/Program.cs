254a86afa2cc76ae2020bd6b18cc521bfd7940e5
Pr9269 <pr9269@g.rit.edu>
2021-09-27 20:38:37 -0400

    PE11 - Q6

---
+++ PE11 - Q6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PE11___Q5;
+
+namespace Traffic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddPassenger(new Compact());
+            AddPassenger(new SUV());
+            ReadKey();
+        }
+        static void AddPassenger(IPassengerCarrier Vehicle)
+        {
+            WriteLine(Vehicle.ToString());
+        }
+    }
+}
