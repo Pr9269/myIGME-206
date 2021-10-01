b4e3e82626d0c491ab6af37209f8b3af4eda3693
Pr9269 <pr9269@g.rit.edu>
2021-10-01 19:39:53 -0400

    PE12 - Q3

---
+++ PE12 - Q3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE12___Q3
+{
+    //Author: Parth Rustagi
+    //Purpose: To derive a class from base class and call one of its string
+    class MyClass
+    {
+        protected string myString;
+        public string ABC
+        {
+            set
+            {
+                myString = value;
+            }
+        }
+        public virtual string GetString() => myString;
+    }
+
+    class MyDerivedClass : MyClass
+    {
+        public override string GetString() => base.GetString() + "output from derived class";
+
+        //Main Program
+        public static void Main(string[] args)
+        {
+            MyDerivedClass obj1 = new MyDerivedClass();
+            Console.WriteLine(obj1.GetString());
+        }
+    }
+}
