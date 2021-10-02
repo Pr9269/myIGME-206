f0c48f5c767eccf102a35898ac4c20d946f62f95
Pr9269 <pr9269@g.rit.edu>
2021-10-01 21:29:08 -0400

    PE12 - Q7

--- PE12 - Q5/Couxrse.cs
+++ PE12 - Q5/Couxrse.cs
@@ -5,7 +5,7 @@
         private string v1;
         private string v2;
 
-        public Couxrse(string v1, string v2)
+        public Course(string v1, string v2)
         {
             this.v1 = v1;
             this.v2 = v2;
