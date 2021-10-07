e0638eae9676bc61bfa0635f6debd5139da9b646
Pr9269 <pr9269@g.rit.edu>
2021-10-06 21:44:42 -0400

    PE14 - Q3

---
+++ PE14 - Q3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PE14___Q2;
+
+namespace PE14___Q3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            pe14Class1 obj1 = new pe14Class1();
+            pe14Class2 obj2 = new pe14Class2();
+            MyMethod(obj1);
+            MyMethod(obj2);
+        }
+
+        public static void MyMethod(PE1 myObject)
+        {
+            myObject.Method();
+        }
+    }
+}
