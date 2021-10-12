4ff5916f2e8df90e1f0af060cd9c1008f6a32762
Pr9269 <pr9269@g.rit.edu>
2021-10-11 23:05:39 -0400

    PE16

---
+++ PE15/Class1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE15
+{
+    public interface ICustomer
+    {
+        void Greet();
+    }
+
+    public class Customer : ICustomer
+    {
+        public string name;
+        private string creditCardNumber;
+        public string CreditCardNumber
+        {
+            set
+            {
+                this.creditCardNumber = value;
+            }
+        }
+
+        public void Greet()
+        {
+            Console.WriteLine("Hello!");
+        }
+    }
+
+    public abstract class HotDrink
+    {
+        public bool instant;
+        public bool milk;
+        private byte sugar;
+        public string brand;
+        public string size;
+
+        public Customer customer;
+
+        public HotDrink()
+        {
+            this.instant = false;
+            this.milk = false;
+            this.sugar = 0;
+            this.size = "medium";
+
+            this.customer = new Customer();
+        }
+
+        public HotDrink(string brand)
+        {
+            // Folgers is instant coffee
+            if (brand == "Folgers")
+            {
+                this.instant = true;
+            }
+
+            this.brand = brand;
+
+            this.customer = new Customer();
+        }
+
+        public virtual void AddSugar(byte amount)
+        {
+            sugar += amount;
+        }
+
+        public abstract void Steam();
+    }
+
+    public class CupOfCoffee : HotDrink
+    {
+        public string beanType;
+
+        public CupOfCoffee()
+        {
+
+        }
+
+        public CupOfCoffee(string brand) : base(brand)
+        {
+            if (brand == "Folgers")
+            {
+                this.beanType = "rancid";
+            }
+        }
+
+        public override void Steam()
+        {
+        }
+    }
+
+}
---
+++ PE16/Class1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE16
+{
+    public abstract class HotDrink
+    {
+        public bool instant;
+        public bool milk;
+        private byte sugar;
+        public string size;
+        public Customer customer;
+
+        public virtual void AddSugar(byte amount)
+        {
+
+        }
+        
+        public abstract void Steam();
+
+        public HotDrink()
+        {
+
+        }
+
+        public HotDrink(string brand)
+        {
+
+        }
+    }
+
+    public interface IMood
+    {
+        public string Mood
+        {
+            get;
+        }
+    }
+
+    public interface ITakeOrder
+    {
+        void TakeOrder();
+    }
+
+    public class Waiter : IMood
+    {
+        public string name;
+        public string Mood
+        {
+            get;
+        }
+
+        public void ServerCustomer(HotDrink cup)
+        {
+
+        }
+    }
+
+    public class Customer : IMood
+    {
+        public string name;
+        public string creditCardNumber;
+
+        public string Mood
+        {
+            get;
+        }
+    }
+
+    public class CupOfCoffee : HotDrink, ITakeOrder
+    {
+        public string beanType;
+        public override void Steam()
+        {
+
+        }
+
+        public void TakeOrder()
+        {
+
+        }
+
+        public CupOfCoffee(string brand) : base(brand)
+        {
+
+        }
+    }
+
+    public class CupOfTea : HotDrink, ITakeOrder
+    {
+        string leafType;
+        public override void Steam()
+        {
+
+        }
+
+        public void TakeOrder()
+        {
+
+        }
+
+        public CupOfTea(bool customerIsWealthy)
+        {
+
+        }
+    }
+
+    public class CupOfCocoa : HotDrink, ITakeOrder
+    {
+        public static int numCups;
+        public bool marshmallow;
+        private string source;
+
+        public string Source
+        {
+            get;
+            set;
+        }
+
+        public override void Steam()
+        {
+
+        }
+
+        public override void AddSugar(byte amount)
+        {
+
+        }
+
+        public void TakeOrder()
+        {
+
+        }
+
+        public CupOfCocoa() : this(false)
+        {
+
+        }
+
+        public CupOfCocoa(bool marshmallows) : base("Expensive Organic Brand")
+        {
+
+        }
+    }
+
+}
