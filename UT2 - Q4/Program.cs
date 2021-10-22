150e387dff6aba4fcb2792d45caa15c3cb14bf60
Pr9269 <pr9269@g.rit.edu>
2021-10-22 19:14:54 -0400

    UT2 - Q7

--- UT2 - Q4/Program.cs
+++ UT2 - Q4/Program.cs
@@ -101,7 +101,105 @@ namespace UT2___Q4
 
         public void TimeTravel()
         {
+            Console.WriteLine("*************** Time Travel ***************");
+        }
 
+        public static bool operator ==(Tardis t1, Tardis t2)
+        {
+            bool isTrueorNot = false;
+            
+            if (t1.whichDrWho == t2.whichDrWho)
+            {
+                isTrueorNot = true;
+            
+            }
+            return isTrueorNot;
+        }
+        public static bool operator !=(Tardis t1, Tardis t2)
+        {
+            bool isTrueorNot = false;
+            if (t1.whichDrWho != t2.whichDrWho)
+            {
+
+                isTrueorNot = true;
+            }
+            return isTrueorNot;
+        }
+        public static bool operator <(Tardis t1, Tardis t2)
+        {
+            bool isTrueorNot = false;
+            if (t1.whichDrWho == 10)
+            {
+                isTrueorNot = false;
+            }
+            else if (t2.whichDrWho == 10)
+            {
+                isTrueorNot = true;
+            }
+            else if (t1.whichDrWho < t2.whichDrWho)
+            {
+                isTrueorNot = true;
+            }
+            return isTrueorNot;
+        }
+        public static bool operator >(Tardis t1, Tardis t2)
+        {
+            bool isTrueorNot = false;
+            if (t1.whichDrWho == 10)
+            {
+                isTrueorNot = true;
+            }
+            else if (t2.whichDrWho == 10)
+            {
+                isTrueorNot = false;
+            }
+            else if (t1.whichDrWho > t2.whichDrWho)
+            {
+                isTrueorNot = true;
+            }
+            return isTrueorNot;
+        }
+        public static bool operator <=(Tardis t1, Tardis t2)
+        {
+            bool isTrueorNot = false;
+            if (t1.whichDrWho == 10)
+            {
+                isTrueorNot = false;
+            }
+            else if (t2.whichDrWho == 10)
+            {
+                isTrueorNot = true;
+            }
+            else if (t1.whichDrWho <= t2.whichDrWho)
+            {
+                isTrueorNot = true;
+            }
+            return isTrueorNot;
+        }
+        public static bool operator >=(Tardis t1, Tardis t2)
+        {
+            bool isTrueorNot = false;
+            if (t1.whichDrWho == 10)
+            {
+                isTrueorNot = true;
+            }
+            else if (t2.whichDrWho == 10)
+            {
+                isTrueorNot = false;
+            }
+            else if (t1.whichDrWho >= t2.whichDrWho)
+            {
+                isTrueorNot = true;
+            }
+            return isTrueorNot;
+        }
+        public override int GetHashCode()
+        {
+            return 0;
+        }
+        public override bool Equals(object o)
+        {
+            return true;
         }
     }
 
@@ -113,12 +211,40 @@ namespace UT2___Q4
 
         public void OpenDoor()
         {
-
+            Console.WriteLine("*************** Open door ***************");
         }
 
         public void CloseDoor()
         {
 
+        }
+    }
+
+    public class runtime
+
+    {
+
+        public static void Main(string[] args)
+        {
+            PhoneInterface t1 = new Tardis();
+            PhoneInterface p1 = new PhoneBooth();
+
+            UsePhone(t1);
+            UsePhone(p1);
+
+        }
+        static void UsePhone(object obj)
+        {
+            ((PhoneInterface)obj).MakeCall();
+            ((PhoneInterface)obj).HangUp();
+            try
+            {
+                ((Tardis)obj).TimeTravel();
+            }
+            catch
+            {
+                ((PhoneBooth)obj).OpenDoor();
+            }
         }
     }
 }
---
+++ UT2 - Q9/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UT2___Q9
+{
+    public abstract class Guitar
+    {
+        public string model;
+        public string company;
+        public string type;
+
+        public virtual void Strumming()
+        {
+
+        }
+    }
+
+    public interface IAcoustic
+    {
+        void Party();
+    }
+
+    public interface IElectric
+    {
+        void Play();
+    }
+
+    public class Acoustic : Guitar, IAcoustic
+    {
+        public bool capo;
+
+        public void Play()
+        {
+
+        }
+
+        public void Party()
+        {
+
+        }
+    }
+
+    public class Electric : Guitar, IElectric
+    {
+        public string speciality;
+
+        public void Strumming()
+        {
+
+        }
+
+        public void Play()
+        {
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
