e289fb804193c1cc3948c4d74a2bbc759ba529a9
Pr9269 <pr9269@g.rit.edu>
2021-12-15 19:58:51 -0500

    FT - Q1

---
+++ Final Exam/FT-Q1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question1_Final_gaw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AlphabetCounter();
+        }
+
+        static void AlphabetCounter()
+        {
+            string stringInput;
+            int[] characterCount = new int[50];
+            int index = 0;
+
+            Console.Write("Please input a string: ");
+            stringInput = Console.ReadLine();
+
+
+            foreach (char c in stringInput.ToLower())
+            {
+                if (Char.IsLetter(c))
+                {
+                    ++characterCount[c - 'a'];
+                }
+            }
+
+            while( index < characterCount.Length)
+            {
+                Console.WriteLine($"{(char)(index + 'a')}: {characterCount[index]}");
+                index++;
+            }
+        }
+    }
+}
